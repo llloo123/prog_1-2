655a2641587d352417d6b44a9e8ce96f4c49c00b
llloo123 <htsmslk@gmail.com>
2024-10-07 10:31:26 +0500

    Update lab1.c

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 
+int calc_sum_digits(int num)
+{
+    int sum_digits = 0;
+    while(num != 0) {
+        sum_digits += num % 10;
+        num = num / 10;
+    }   
+    return sum_digits;
+}
+
 int main()
 {
-    float a, b, c;
- 
+    int a, b;
+    printf("Enter a -> ");
+    scanf("%d", &a);
+    printf("Enter b -> ");
+    scanf("%d", &b);
+
+    int sum_digits_a, sum_digits_b;
+    sum_digits_a = calc_sum_digits(a);
+    sum_digits_b = calc_sum_digits(b);
+
+    if (sum_digits_a > b)
+        printf("sum_digits_a = %d\n", sum_digits_a);
+    if (sum_digits_a == b)
+        printf("sum_digits_b = %d\n", sum_digits_b);
+    if (sum_digits_a < b)
+       printf("sum_digits_a + b = %d\n", sum_digits_a + b);
+    return 0;
+}
